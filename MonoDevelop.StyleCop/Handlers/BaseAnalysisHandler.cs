8eac375a856fc8440c31c844fa5e65fd561ac085
DarkCloud14 <darkcloud14@gmail.com>
2012-09-19 11:38:38 +0200

    Added FullAnalysis property which will be set to the appropriate value by BaseAnalysisHandlers child classes

--- MonoDevelop.StyleCop/Handlers/BaseAnalysisHandler.cs
+++ MonoDevelop.StyleCop/Handlers/BaseAnalysisHandler.cs
@@ -20,17 +20,55 @@
 //-----------------------------------------------------------------------
 namespace MonoDevelop.StyleCop
 {
-  using System;
   using System.Collections.Generic;
-  using System.Linq;
-  using System.Text;
   using MonoDevelop.Components.Commands;
+  using MonoDevelop.Ide;
+  using global::StyleCop;
 
   /// <summary>
-  /// This class will manage the general StyleCop functionality.
-  /// It doesn't handle the collecting of files which should be scanned.
+  /// This class will manage the basic StyleCop functionality.
   /// </summary>
   internal abstract class BaseAnalysisHandler : CommandHandler
   {
+    #region Protected Properties
+
+    /// <summary>
+    /// Gets or sets a value indicating whether a full analysis should be performed.
+    /// </summary>
+    protected bool FullAnalysis
+    {
+      get;
+      set;
+    }
+
+    #endregion Protected Properties
+
+    #region Protected Methods
+
+    /// <summary>
+    /// Gathers the list of files to analyze and hands it to a ProjectOperation method which will kick off the worker thread.
+    /// </summary>
+    /// <param name="analysisType">The analyze type being performed.</param>
+    protected void Analyze(AnalysisType analysisType)
+    {
+      if (!IdeApp.ProjectOperations.IsStyleCopRunning())
+      {
+        IList<CodeProject> projects = ProjectUtilities.Instance.GetProjectList(analysisType);
+        IdeApp.ProjectOperations.StyleCopAnalysis(IdeApp.ProjectOperations.CurrentSelectedBuildTarget, this.FullAnalysis, projects);
+      }
+    }
+
+    /// <summary>
+    /// Cancel a previously started StyleCop analysis.
+    /// </summary>
+    protected void CancelAnalysis()
+    {
+      if (IdeApp.ProjectOperations.IsStyleCopRunning())
+      {
+        IdeApp.ProjectOperations.CancelStyleCopAnalysis();
+      }
+    }
+
+    #endregion Protected Methods
   }
 }
