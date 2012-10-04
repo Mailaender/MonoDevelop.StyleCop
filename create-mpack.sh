56e48c7aecf2bc007794c1f61255460fd7516817
DarkCloud14 <darkcloud14@gmail.com>
2012-10-04 02:12:15 +0200

    Moved StyleCop files to directory StyleCop.Files and added the latest release build of MonoDevelop.StyleCop.dll. Modified create-mpack.sh and addin-project.xml to reflect the changes.

--- create-mpack.sh
+++ create-mpack.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 MonoDevelopTool=/Applications/MonoDevelop.app/Contents/MacOS/mdtool
-StyleCopFilesDirectory=./StyleCop\ Files
+StyleCopFilesDirectory=./StyleCop.Files
 AddinBuildDirectory=./MonoDevelop.StyleCop/bin/Release
 AddinFileName=MonoDevelop.StyleCop.dll
 AddinFullFileName="$AddinBuildDirectory/$AddinFileName"
@@ -10,7 +10,6 @@ if [ -f "$AddinFullFileName" ]; then
 		if [ -f "$MonoDevelopTool" ]; then		
 	    	cp -f "$AddinFullFileName" "$StyleCopFilesDirectory"
 	    	$MonoDevelopTool setup pack "$StyleCopFilesDirectory/$AddinFileName"
-	    	rm "$StyleCopFilesDirectory/$AddinFileName"
 	    else
 	    	echo "Couldn't find the necessary MonoDevelop tool mdtool. Please make sure the path in this script is set correctly."
 	    fi
