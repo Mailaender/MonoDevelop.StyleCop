fdf3454394ec933cc7012f316f9cccbcb3244da4
DarkCloud14 <darkcloud14@gmail.com>
2012-10-06 00:29:05 +0200

    * MonoDevelop.StyleCop.sln: * DummyClass.cs: * DummyProject.csproj: Added a new Dummy project with dummy class. It's only purpose is to copy the official StyleCop files into   MonoDevelop.StyleCop build directory!
    * NOTICE:
    * LICENSE:
    * LICENSE_Ms-PL.txt:
    * MonoDevelop.StyleCop.dll: Removed douplicated files with new
      solution structure it isn't necessary anymore.

---
+++ StyleCop.Files/DummyClass.cs
@@ -0,0 +1,35 @@
+//-----------------------------------------------------------------------
+// <copyright file="DummyClass.cs">
+//   APL 2.0
+// </copyright>
+// <license>
+//   Copyright 2012 Alexander Jochum
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+// </license>
+//-----------------------------------------------------------------------
+namespace DummyProject
+{
+  /// <summary>
+  /// Dummy class.
+  /// </summary>
+  public class DummyClass
+  {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DummyProject.DummyClass"/> class.
+    /// </summary>
+    public DummyClass()
+    {
+    }
+  }
+}
