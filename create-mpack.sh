325f0456ecffb6e70a9ee9c54d308aaa8f6f2876
DarkCloud14 <darkcloud14@gmail.com>
2012-10-06 00:38:32 +0200

    The script will now do a Release build of the solution and then create a addin package.

--- create-mpack.sh
+++ create-mpack.sh
@@ -1,28 +1,18 @@
 #!/bin/bash
 MonoDevelopTool=/Applications/MonoDevelop.app/Contents/MacOS/mdtool
-StyleCopFilesDirectory=./StyleCop.Files
 AddinBuildDirectory=./MonoDevelop.StyleCop/bin/Release
 AddinFileName=MonoDevelop.StyleCop.dll
 AddinFullFileName="$AddinBuildDirectory/$AddinFileName"
 
-if [ -f "$AddinFullFileName" ]; then
-	if [ -d "$StyleCopFilesDirectory" ]; then
-		if [ -f "$MonoDevelopTool" ]; then
-			if [ -f "$StyleCopFilesDirectory/$AddinFileName" ]; then
-				rm "$StyleCopFilesDirectory/$AddinFileName"	
-			fi
-			
-			cp -f LICENSE "$StyleCopFilesDirectory"
-			cp -f LICENSE_Ms-PL.txt "$StyleCopFilesDirectory"
-			cp -f NOTICE "$StyleCopFilesDirectory"
-	    	cp -f "$AddinFullFileName" "$StyleCopFilesDirectory"
-	    	$MonoDevelopTool setup pack "$StyleCopFilesDirectory/$AddinFileName"
-	    else
-	    	echo "Couldn't find the necessary MonoDevelop tool mdtool. Please make sure the path in this script is set correctly."
-	    fi
-	else
-		echo "Make sure the StyleCop files are in the directory $StyleCopFilesDirectory/ !"
-	fi
+if [ -f "$MonoDevelopTool" ]; then
+  $MonoDevelopTool build -t:Clean -c:Release
+  $MonoDevelopTool build -c:Release
+
+  if [ -f "$AddinFullFileName" ]; then
+      $MonoDevelopTool setup pack "$AddinFullFileName"
+    else
+    echo "Couldn't find $AddinFileName in $AddinBuildDirectory"
+  fi
 else
-	echo "Couldn't find $AddinFileName in $AddinBuildDirectory"
+    echo "Couldn't find the necessary MonoDevelop tool mdtool. Please make sure the path in this script is set correctly."
 fi
