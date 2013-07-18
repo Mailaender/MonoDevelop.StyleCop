c0ba77954eb9b94e6b9b01e6fbd9cad92bc1a9e8
DarkCloud14 <darkcloud14@gmail.com>
2013-07-18 17:41:36 +0200

    * MonoDevelop.StyleCop.sln:   Updated version to 1.0.1.5   Set BlankLinesBetweenFields to 1
    * DummyProject.csproj:
    * AssemblyInfo.cs:
    * MonoDevelop.StyleCop.csproj:
      Updated version to 1.0.1.5

--- MonoDevelop.StyleCop/Properties/AssemblyInfo.cs
+++ MonoDevelop.StyleCop/Properties/AssemblyInfo.cs
@@ -40,5 +40,5 @@ using System.Runtime.InteropServices;
 [assembly: CLSCompliant(true)]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
 
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.0.1.5")]
+[assembly: AssemblyFileVersion("1.0.1.5")]
